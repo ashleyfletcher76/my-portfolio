9356a08ca28ceca60ab661f05dd6f1b3282f3b44
Ashley Fletcher <ashley.fletcher76@yahoo.com>
2026-01-06 16:50:32 +0100

    About link updated

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import Link from 'next/link';
+import { Link } from '@/i18n/routing';
 import { getLocale, getTranslations } from 'next-intl/server';
 
 import CVCard from '@/components/CVCard';
